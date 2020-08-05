95e852746669b99e9ba2d87b3a037a7dc6aeceb9
jaydenchuljinlee <cheoljin1234@naver.com>
2020-08-05 22:52:10 +0900

    as-is component

--- src/main.js
+++ src/main.js
@@ -1,22 +1,3 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-var example1 = new Vue({
-  el: '#example-1',
-  data: {
-    counter: 0
-  }
-})
-
-var example2 = new Vue({
-  el: '#example-3',
-  methods: {
-    say: function(message) {
-      alert(message)
-    },
-    warn: function(message,event) {
-      if (event) event.preventDefault()
-      alert(message)
-    }
-  }
-})
